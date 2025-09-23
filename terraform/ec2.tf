0ba0e3ea765527dafc8ecbb8b360068dcc5003be
sairamreddy9666 <115546095+sairamreddy9666@users.noreply.github.com>
2025-09-23 20:59:10 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "strapi" {
   vpc_security_group_ids = [aws_security_group.strapi_sg.id]
 
   user_data = templatefile("${path.module}/user_data.tpl", {
-    image_url = "${aws_ecr_repository.strapi.repository_url}:${var.image_tag}"
+    image_url = "${data.aws_ecr_repository.strapi.repository_url}:${var.image_tag}"
   })
 
   tags = {
