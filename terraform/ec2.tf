5c7395175f19ea6211624f32785aa751bc2a81e8
sairamreddy9666 <115546095+sairamreddy9666@users.noreply.github.com>
2025-09-24 11:19:52 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,9 +1,20 @@
+data "aws_security_group" "strapi_sg" {
+  filter {
+    name   = "group-name"
+    values = ["strapi-sg"]
+  }
+  vpc_id = "vpc-01b35def73b166fdc"
+}
+
 resource "aws_instance" "strapi" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
   availability_zone = "ap-south-1a"
-  vpc_security_group_ids      = ["sg-0abc1234def5678gh"]
+  subnet_id     = "subnet-0393e7c5b435bd5b6"
+
+
+  vpc_security_group_ids = [aws_security_group.strapi_sg.id]
 
   user_data = templatefile("${path.module}/user_data.tpl", {
     image_url = "${data.aws_ecr_repository.strapi.repository_url}:${var.image_tag}"
